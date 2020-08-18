ef6c187ce579fe0ca83cfd2e721bf93fc4a356a2
OnurBaris <62941036+OnurBaris@users.noreply.github.com>
2020-08-18 18:16:53 +0300

    Add files via upload
    Correlation matrix plotted and some features are eliminated

---
+++ HousePrices_Kaggle_Regression_corrmatrix.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 11:49:37 2020
+
+@author: obaris
+"""
+
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+        
+train_data = pd.read_csv("C:/01_Projects/09_CriticalFormulasandTools/PythonScripts/Kaggle_HousePrices/train.csv")
+test_data = pd.read_csv("C:/01_Projects/09_CriticalFormulasandTools/PythonScripts/Kaggle_HousePrices/test.csv")
+train_data.set_index('Id')
+test_data.set_index('Id')
+
+condition = ['KitchenQual', 'PoolQC', 'GarageCond', 'BsmtCond', 'FireplaceQu', 'HeatingQC', 'BsmtQual', 'GarageQual', 'ExterQual', 'ExterCond']
+
+for i in condition:
+    train_nan = train_data.loc[:,i].isna()
+    test_nan = test_data.loc[:,i].isna()
+    for j in train_data.index:
+        if train_data.loc[j,i]=='Gd':
+            train_data.loc[j,i]=0.8
+        elif train_data.loc[j, i]=='Po':
+            train_data.loc[j,i]=0.2
+        elif train_data.loc[j, i]=='Ex':
+            train_data.loc[j, i]=1.0
+        elif train_data.loc[j, i]=='Fa':
+            train_data.loc[j, i]=0.4
+        elif train_data.loc[j, i]=='TA':
+            train_data.loc[j, i]=0.6
+        elif train_data.loc[j, i]=='NA' or train_data.loc[j, i]=='na' or train_nan[j]==True:
+            train_data.loc[j, i]=0.0
+    for z in test_data.index:
+        if test_data.loc[z,i]=='Gd':
+            test_data.loc[z,i]=0.8
+        elif test_data.loc[z, i]=='Po':
+            test_data.loc[z,i]=0.2
+        elif test_data.loc[z, i]=='Ex':
+            test_data.loc[z, i]=1.0
+        elif test_data.loc[z, i]=='Fa':
+            test_data.loc[z, i]=0.4
+        elif test_data.loc[z, i]=='TA':
+            test_data.loc[z, i]=0.6
+        elif test_data.loc[z, i]=='NA' or test_data.loc[z, i]=='na' or test_nan[z]==True:
+            test_data.loc[z, i]=0.0
+
+corrmat = train_data.corr(method='spearman')
+f, ax = plt.subplots(figsize=(12, 10))
+sns.heatmap(corrmat, ax=ax, annot=True, fmt=".1f", annot_kws={'size':8}, center=0, linewidths=0.1)
+
+# Looking at the correlation matrix Id, MSSubClass, BsmtFinSF2, LowQualFinSF, BsmtHalfBath, 35snPorch, ScreenPorch, PoolArea
+# MiscVal have weak correlation to price sold
